d0774a82c9f7f7fa280c35079928194e9ffd4a3f
ThomasChen <canchen@usc.edu>
2019-10-26 04:13:11 -0700

    some core mechanics done, but there's a lag when player's jumping

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    Transform mTarget;
+    Player mPlayer;
+    GameObject mPlanet;
+    public float smoothTime = 0.3F;
+    private Vector3 velocity = Vector3.zero;
+
+    private float desiredRot;
+    public float damping = 10;
+    public float inAirDamping = 1.5f;
+
+    private void Start()
+    {
+        mPlayer = GameObject.Find("Player").GetComponent<Player>();
+        mTarget = mPlayer.transform;
+    }
+
+    void Update()
+    {
+        // Define a target position above and behind the target transform
+        Vector3 targetPosition = new Vector3(mTarget.position.x, mTarget.position.y, -10);
+
+        // Smoothly move the camera towards that target position
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+        mPlanet = mPlayer.mPlanet;
+        Debug.Log(mPlanet.name);
+        if (mPlanet == null) return;
+
+        Vector3 fromPlanet = mTarget.position - mPlanet.transform.position;
+        float desiredRot = Mathf.Atan2(fromPlanet.y, fromPlanet.x) * Mathf.Rad2Deg - 90;
+
+        float currDamp;
+        if (mPlayer.grounded)
+        {
+            currDamp = damping;
+        }
+        else {
+            currDamp = inAirDamping;
+        }
+        var desiredRotQ = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, desiredRot);
+        transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotQ, Time.deltaTime * currDamp);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,26 +5,92 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float mSpeed = 1;
+    public float jumpForce = 5;
+
+    CameraFollow mCamera;
     Rigidbody2D mBody;
-    GameObject mPlanet;
+    [HideInInspector]public GameObject mPlanet;
+
+    [HideInInspector]public bool grounded = false;
+    float jumpOffset = 0.1f;
+    Vector3 mRunVelocity;
+    List<Collider2D> mGravities = new List<Collider2D>();
     // Start is called before the first frame update
     void Start()
     {
-        mBody = GetComponent<Rigidbody2D>();   
+        mBody = GetComponent<Rigidbody2D>();
+        mCamera = Camera.main.GetComponent<CameraFollow>();
     }
     // Update is called once per frame
     void Update()
     {
-        Move();
+        
+
     }
-    void Move() {
-        float move = Input.GetAxis("Horizontal") * Time.deltaTime * mSpeed;
-        Vector3 movement = transform.right * move;
-        if (movement.magnitude > 0)
+    private void FixedUpdate()
+    {
+        SetMyPlanet();
+        if (grounded)
         {
-            Debug.Log(transform.right);
+            Run();
         }
-        transform.Translate(movement);
     }
-    //void 
+    void Run() {
+        mRunVelocity = transform.right * Input.GetAxis("Horizontal") * mSpeed ;
+        transform.position += mRunVelocity * Time.fixedDeltaTime;
+
+        if (Input.GetKeyDown("space"))
+        {
+            //Debug.Log("Jump!");
+            transform.position += transform.up * jumpOffset;
+            mBody.velocity = (transform.up * jumpForce + mRunVelocity).normalized*jumpForce;
+            grounded = false;
+        }
+    }
+    void SetMyPlanet() {
+        float minDis = float.PositiveInfinity;
+        foreach(Collider2D c in mGravities) {
+            float dis = (c.transform.position - transform.position).magnitude;
+            GameObject p = c.gameObject.transform.parent.gameObject;
+            dis -= p.transform.localScale.x/2;
+            if ( dis < minDis) {
+                minDis = dis;
+                mPlanet = c.gameObject.transform.parent.gameObject;
+            }
+        }
+
+        Vector3 fromPlanet = transform.position- mPlanet.transform.position ;
+        float angle = Mathf.Atan2(fromPlanet.y, fromPlanet.x) * Mathf.Rad2Deg-90;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+    void SnapToGround() {
+        if (mPlanet == null) return;
+        Vector3 dif = transform.position - mPlanet.transform.position;
+        dif.Normalize();
+        float groundDis = (mPlanet.transform.localScale.x + transform.localScale.y) / 2;
+        transform.position = mPlanet.transform.position + dif * groundDis;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+        if (mPlanet == collision.gameObject) {
+            //Debug.Log(collision.collider.name);
+            mBody.velocity = Vector3.zero;
+            SnapToGround();
+            grounded = true;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "GravityFeild") {
+            mGravities.Add(other);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "GravityFeild")
+        {
+            mGravities.Remove(other);
+        }
+    }
 }
