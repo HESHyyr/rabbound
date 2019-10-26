19eb0c23cd452b853806e8039e0b3ddcdceb698e
glacay30 <glacayo@usc.edu>
2019-10-26 11:37:00 -0700

    Lacayo - Add initial wave parameters and movement
    In inspector, able to set the origin, angle, and speed of the
    Wave game object and it will move in the direction of the given
    angle at the given speed.

--- Assets/Scripts/WaveMovement.cs
+++ Assets/Scripts/WaveMovement.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class WaveMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Vector2 origin = Vector3.zero;
+    [SerializeField] private float angle = 0.0f;
+    [SerializeField] private float speed = 0.0f;
+
+    private Rigidbody2D rb = null;
+
+    // Inherited from MonoBehavior
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+
+        transform.position = origin;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        SetTransformWithAngle();
+        MoveWave();
+
+        DrawDebugLines();
+    }
+
+    // Methods
+
+    private void SetTransformWithAngle()
+    {
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+    private void MoveWave()
+    {
+        rb.velocity = transform.up * speed;
+    }
+
+    private void DrawDebugLines()
+    {
+        Debug.DrawLine(transform.position, transform.position + transform.up, Color.green);
+        Debug.DrawLine(transform.position, transform.position + transform.right, Color.red);
     }
 }
