0caba77c56057ea6e31512c16f27990c0afd6180
glacay30 <glacayo@usc.edu>
2019-10-26 18:23:35 -0700

    Lacayo - Add parallax to stars
    Background.cs handles the children stars that would be attached
    to the Background game object in the scene.
    
    Firstly, Start grabs the transform of each star and pairs it with
    a parallax coefficient that is the percentage the star will move
    with the camera.
    
    In Update, it iterates over all stars and updates their positions
    based on the camera movement. If a star were to leave the frame,
    it will spawn on the other side of that frame.

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using ParallaxCoefficient = System.Single;
+
+public class Background : MonoBehaviour
+{
+    [SerializeField] private GameObject player = null;
+
+    [SerializeField] private ParallaxCoefficient initial = 0.8f;
+
+    private class ParallaxData
+    {
+        public Transform transform = null;
+        public ParallaxCoefficient coef = 0.0f;
+    }
+
+    private List<ParallaxData> stars = new List<ParallaxData>();
+
+    private Vector3 prevCameraPos = Vector3.zero;
+
+    // inherited from monobehavior
+
+    private void Start()
+    {
+        FillStarData();
+    }
+
+    private void Update()
+    {
+        transform.position = (Vector2)Camera.main.transform.position;
+
+        UpdateStars();
+
+        prevCameraPos = Camera.main.transform.position;
+    }
+
+    // private methods
+
+    private void FillStarData()
+    {
+        foreach (Transform child in transform) {
+            ParallaxData data = new ParallaxData {
+                transform = child,
+                coef = Random.Range(0.0f, initial)
+            };
+            stars.Add(data);
+        }
+    }
+
+    private void UpdateStars()
+    {
+        foreach (ParallaxData star in stars) {
+
+            Vector3 camChange = prevCameraPos - Camera.main.transform.position;
+
+            Vector3 newPos = star.transform.position;
+            newPos.x -= camChange.x * star.coef;
+            newPos.y -= camChange.y * star.coef;
+
+            star.transform.position = WarpStarFromScreen(newPos);
+        }
+    }
+
+    private Vector2 WarpStarFromScreen(Vector2 pos)
+    {
+        Vector3 warpedPosition = pos;
+
+        Vector3 posInViewport = Camera.main.WorldToViewportPoint(pos);
+
+        float height = Camera.main.orthographicSize * 2;
+        float width = height * Camera.main.aspect;
+
+        if (posInViewport.x < 0.0f) {
+            // left of screen
+            warpedPosition += Camera.main.transform.right * width;
+        }
+        else if (posInViewport.x > 1.0f) {
+            // right of screen
+            warpedPosition -= Camera.main.transform.right * width;
+        }
+
+        if (posInViewport.y < 0.0f) {
+            // below screen
+            warpedPosition += Camera.main.transform.up * height;
+        }
+        else if (posInViewport.y > 1.0f) {
+            // above screen
+            warpedPosition -= Camera.main.transform.up * height;
+        }
+
+        return warpedPosition;
+    }
+}
